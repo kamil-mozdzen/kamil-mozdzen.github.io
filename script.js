3fb68fab13f63bfcf1e993081e013764db9d875e
kamil-mozdzen <123424205+kamil-mozdzen@users.noreply.github.com>
2024-02-18 23:27:46 +0100

    Adding checkDevice function

--- script.js
+++ script.js
@@ -97,6 +97,27 @@ function startCam() {   //
 }
 startCam(); //call startCam method
 */
+
+console.log("MOBILE DEVICE");
+console.log(checkDevice());
+
+function checkDevice(){
+    var a;
+   console.log(window.navigator.userAgent);
+       if (navigator.userAgent.match(/Android/i)
+       || navigator.userAgent.match(/webOS/i)
+       || navigator.userAgent.match(/iPhone/i)
+       || navigator.userAgent.match(/iPad/i)
+       || navigator.userAgent.match(/iPod/i)
+       || navigator.userAgent.match(/BlackBerry/i)
+       || navigator.userAgent.match(/Windows Phone/i)) {
+          a = true ;
+       } else {
+          a = false ;
+       } 
+     
+    return a;
+}
 var a = true;
 document.onload = function() //is executed when the page is fully loaded
 {
