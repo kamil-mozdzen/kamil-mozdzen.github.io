53b707da4f1097f1b31e0b3de0de8e16913fe65e
kamil-mozdzen <123424205+kamil-mozdzen@users.noreply.github.com>
2024-02-26 22:47:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ document.onload = function()
 // Function to redraw the canvas
 function redraw()  //redraw
 {
-    videoCanvas.getContext('2d').drawImage(video, 0, 0);
+    videoCanvas.getContext('2d').drawImage(video, 0, 0, 640, 480);
     canvas.getContext('2d').drawImage(videoCanvas, 0, 0, width, height);
     // Tell JSARToolKit that the canvas has changed.
     canvas.changed = true;
